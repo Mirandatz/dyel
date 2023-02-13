ae626601ebc5bcc1b047220a297e8bccd0e53c6e
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2023-02-13 00:19:09 -0300

    Further refactored storage

--- dyel/storage.py
+++ dyel/storage.py
@@ -1,5 +1,7 @@
 import pydantic
 
+from minio import Minio, S3Error  # type: ignore
+
 
 class MinIOSettings(pydantic.BaseSettings):
     endpoint: str = pydantic.Field(
@@ -12,6 +14,7 @@ class MinIOSettings(pydantic.BaseSettings):
     )
     user: str = pydantic.Field(..., env="MINIO_ROOT_USER")
     password: str = pydantic.Field(..., env="MINIO_ROOT_PASSWORD")
+    secure: bool = pydantic.Field(False, env="MINIO_SECURE")
 
     # pydantic magic
     class Config:
@@ -19,3 +22,40 @@ class MinIOSettings(pydantic.BaseSettings):
         validate_assignment = True
         extra = pydantic.Extra.forbid
         env_file = "secrets/secrets.env"
+
+
+class MinIOClient:
+    def __init__(self, settings: MinIOSettings) -> None:
+        self._minio = Minio(
+            endpoint=settings.endpoint,
+            access_key=settings.user,
+            secret_key=settings.password,
+            secure=settings.secure,
+        )
+
+    def is_object_stored(self, bucket_name: str, object_name: str) -> bool:
+        try:
+            self._minio.stat_object(
+                bucket_name=bucket_name,
+                object_name=object_name,
+            )
+            return True
+
+        except S3Error as ex:
+            if ex.code == "NoSuchKey":
+                return False
+            else:
+                raise
+
+    def store_object(
+        self,
+        bucket_name: str,
+        object_name: str,
+        data: bytes,
+    ) -> None:
+        _ = self._minio.put_object(
+            bucket_name=bucket_name,
+            object_name=object_name,
+            data=data,
+            length=len(data),
+        )
